ed283b3467a7a27e4df888b2aa14295c16d2ee82
Karol Moroz <k.j.moroz@gmail.com>
2022-09-24 23:54:30 +0200

    Stub routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
+  import { Router, Route } from "svelte-navigator";
+  import Dashboard from "./views/dashboard/Dashboard.svelte";
+  import LoginPage from "./views/sessions/LoginPage.svelte";
 </script>
 
-<main>
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
+<Router>
+  <Route path="/" component={Dashboard} />
+  <Route path="/login" component={LoginPage} />
+</Router>
