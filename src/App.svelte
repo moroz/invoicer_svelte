f3e601bb87b41e60b6e84bf61c6c13c5fdcdb2e3
Karol Moroz <k.j.moroz@gmail.com>
2022-09-24 23:57:58 +0200

    Install apollo client

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import client from "./api/client";
+  import { setClient } from "svelte-apollo";
   import { Router, Route } from "svelte-navigator";
   import Dashboard from "./views/dashboard/Dashboard.svelte";
   import LoginPage from "./views/sessions/LoginPage.svelte";
+
+  setClient(client);
 </script>
 
 <Router>
